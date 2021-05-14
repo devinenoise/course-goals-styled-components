7daa25c8764413edef299829d546ef5aadcf19b8
Kyle Devine <devinenoise@gmail.com>
2021-05-14 15:48:00 -0700

    refactor

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 
+// styled component
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
