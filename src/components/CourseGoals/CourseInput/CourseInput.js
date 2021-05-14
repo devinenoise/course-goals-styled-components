ff678d799930595cf170ab653cb2bfae5bcb7317
Kyle Devine <devinenoise@gmail.com>
2021-05-14 15:18:43 -0700

    init

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    // resets the red text/border when typing
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    // trim removes excess whitespace at beginning/end
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      {/* conditional styling based on state of isValid */}
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type='text' onChange={goalInputChangeHandler} />
+      </div>
+      <Button type='submit'>Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
